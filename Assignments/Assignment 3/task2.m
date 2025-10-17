30b0116dc7948756cf6e3e40a69023fd1a83360e
Theo Gustavsson <theojohnhenry@gmail.com>
2025-10-17 17:14:30 +0200

    plots it nicely

--- Assignments/Assignment 3/task2.m
+++ Assignments/Assignment 3/task2.m
@@ -27,15 +27,31 @@ N = round(tf/dt); %antal steg
 
 
 xlin = 0:dt:tf-dt;
-scatter(xlin,mf_f(xlin),'blue');
+%scatter(xlin,mf_f(xlin),'blue');
 hold on;
 
-x_rk = [x0];
+x_rk1 = [x0];
+x_rk2 = [x0];
+x_rk4 = [x0];
+
 
 for k=1:N-1
-   xnext = rk_step(mf_f, x_rk(k),dt,rk4_a, rk4_b, rk4_c);
-   x_rk = [x_rk, xnext];
+   xnext1 = rk_step(mf_f, x_rk1(k),dt,rk1_a, rk1_b, rk1_c);
+   x_rk1 = [x_rk1, xnext1];
+   
+   xnext2 = rk_step(mf_f, x_rk2(k),dt,rk2_a, rk2_b, rk2_c);
+   x_rk2 = [x_rk2, xnext2];
+
+   xnext4 = rk_step(mf_f, x_rk4(k),dt,rk4_a, rk4_b, rk4_c);
+   x_rk4 = [x_rk4, xnext4];
+
 end
 
-scatter(xlin, x_rk,'red')
-plot(xlin, exp(lambda*xlin))
+scatter(xlin, x_rk1,'red')
+hold on;
+scatter(xlin, x_rk2,'green')
+hold on;
+scatter(xlin, x_rk4,'blue')
+hold on;
+
+plot(xlin, exp(lambda*xlin), 'yellow')
